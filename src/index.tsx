070b22945b5ef6432ce1ca19eb5310588da3889c
claudiogaalvao <claudiogaalvao@gmail.com>
2021-05-15 19:25:50 -0300

    Uso de data base do MirageJs

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  // Para conectar de alguma forma o método get e o post, para quando for
+  // cadastrada uma nova transação, termos a listagem atualizada, usamos o 
+  // recurso de banco de dados do MirageJs
+  // O seeds inicializa o nosso banco de dados com alguns valores
+  models: {
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00'),
+        }
+      ],
+    })
+  },
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    });
+
+    this.post('transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
     })
   }
 })
