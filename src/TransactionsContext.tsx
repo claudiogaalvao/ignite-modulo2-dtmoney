fdc0fcfd356fe9f00c466fdddfd3ca88a189d845
claudiogaalvao <claudiogaalvao@gmail.com>
2021-05-15 21:33:11 -0300

    Insere nova transação via post e ContextAPI

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, createContext, ReactNode } from "react";
 import { api } from "./services/api";
 
-interface ITransaction {
+interface Transaction {
     id: number;
     title: string;
     amount: number;
@@ -10,23 +10,56 @@ interface ITransaction {
     createdAt: string;
 }
 
+// interface TransactionInput {
+//     title: string;
+//     amount: number;
+//     type: string;
+//     category: string;
+// }
+
+// Sintaxe TypeScript, que é uma forma de se basear em outra interface que 
+// teria as mesmas propriedades, omitindo algumas. 
+// Seria o mesmo que a interface acima
+// type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+// Forma contrária ao Omit
+type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category'>
+
 interface TransactionProviderProps {
     children: ReactNode;
 }
 
-export const TransactionsContext = createContext<ITransaction[]>([]);
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
 
+export const TransactionsContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+);
 
 export function TransactionsProvider({ children }: TransactionProviderProps) {
-    const [transactions, setTransaction] = useState<ITransaction[]>([]);
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
 
     useEffect(() => {
         api.get('/transactions')
-        .then(response => setTransaction(response.data.transactions));
+        .then(response => setTransactions(response.data.transactions));
     }, []);
 
+    async function createTransaction(transactionInput: TransactionInput) {
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date()
+        });
+        const { transaction } = response.data;
+
+        console.log(transaction);
+
+        setTransactions([...transactions, transaction]);
+    }
+
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{ transactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>
     );
